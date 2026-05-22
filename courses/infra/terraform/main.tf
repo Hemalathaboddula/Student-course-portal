695748730c5e32cafa42803b0abb97498d771bee
Hemalathaboddula <hemalathaboddula2002@gmail.com>
2026-05-22 12:12:16 +0530

    terraform & ansible work completed

--- courses/infra/terraform/main.tf
+++ courses/infra/terraform/main.tf
@@ -2,57 +2,16 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# ✅ Security Group
-resource "aws_security_group" "web_sg" {
-  name = "learnhub-sg"
-
-  # SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP (nginx)
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Django (optional)
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# ✅ EC2 Instance
-resource "aws_instance" "app_server" {
-  ami           = "ami-0e12ffc2dd465f6e4"   # ✅ Works for your account (you can change later)
-  instance_type = "t3.micro"               # ✅ works for you (since script worked)
-  key_name      = "app-key"                # ✅ your new key
-
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+resource "aws_instance" "web" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t3.micro"
+  key_name      = var.key_name
 
   tags = {
-    Name = "LearnHubServer"
+    Name = "devops-ec2"
   }
 }
 
-# ✅ Output Public IP
 output "public_ip" {
-  value = aws_instance.app_server.public_ip
+  value = aws_instance.web.public_ip
 }
